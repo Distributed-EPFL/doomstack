fd64ae4a6dd12b2315c1dd623eda9ac829fbd45c
Matteo Monti <matteo.monti@epfl.ch>
2021-10-12 15:15:20 +0200

    Import `Description`

---
+++ src/description.rs
@@ -0,0 +1,47 @@
+use std::fmt;
+use std::fmt::{Debug, Display, Formatter};
+
+#[derive(Clone)]
+pub enum Description {
+    Static(&'static str),
+    Dynamic(String),
+}
+
+impl Description {
+    pub fn as_str(&self) -> &str {
+        match self {
+            Description::Static(description) => &description,
+            Description::Dynamic(description) => &description,
+        }
+    }
+}
+
+impl From<&'static str> for Description {
+    fn from(description: &'static str) -> Self {
+        Description::Static(description)
+    }
+}
+
+impl From<String> for Description {
+    fn from(description: String) -> Self {
+        Description::Dynamic(description)
+    }
+}
+
+impl Display for Description {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), fmt::Error> {
+        match self {
+            Description::Static(description) => write!(f, "{}", description),
+            Description::Dynamic(description) => write!(f, "{}", description),
+        }
+    }
+}
+
+impl Debug for Description {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), fmt::Error> {
+        match self {
+            Description::Static(description) => write!(f, "{:?}", description),
+            Description::Dynamic(description) => write!(f, "{:?}", description),
+        }
+    }
+}
